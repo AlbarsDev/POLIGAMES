d34d68252b08a6b089f83feded6702ccce7afe00
albars <iudicaalba@gmail.com>
2024-03-01 02:33:39 -0400

    WORDSCRABLE ENGLISH WORDS

--- WORDSCRAMBLE/script.js
+++ WORDSCRAMBLE/script.js
@@ -1,74 +1,38 @@
 
-const categorias = {
-    "ANIMALES": [],
-
-    "PARTES DEL CUERPO": [],
-
-    "DULCES": [],
-
-    "ROPA": ['sostén', 'calcetines', 'bombín', 'bikini', 'pantalones', 'gafas', 'sin mangas', 'guantes', 'mono', 'mochila', 'bufanda', 'chal', 'poncho', 'falda', 'cartera', 'zapatillas', 'chaleco', 'suéter', 'sombrero', 'ropa de bebé', 'calzoncillos', 'chanclas', 'zapato', 'cinturón', 'pajarita', 'gorro de invierno', 'bolso', 'gorra', 'sudadera', 'top', 'chaqueta', 'tacones altos', 'reloj', 'vaqueros', 'camisa', 'camisa de manga corta', 'percha', 'vestido', 'corbata', 'traje de baño', 'pantalones cortos'],
-
-    "MIEMBROS DE LA FAMILIA": [],
-
-    "FECHAS Y TIEMPO": ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez', 'once', 'doce', 'trece', 'catorce', 'quince', 'dieciséis', 'diecisiete', 'dieciocho', 'diecinueve', 'veinte', 'veintiuno', 'veintidós', 'veintitrés', 'veinticuatro', 'veinticinco', 'veintiséis', 'veintisiete', 'veintiocho', 'veintinueve', 'treinta', 'treinta y uno', 'calendario', 'un minuto', 'un segundo', 'día', 'mes', 'mañana', 'noche', 'nombres', 'una hora', 'tiempo', 'semana', 'año'],
-
-    "FLORES": [],
-
-    "FRUTAS": [],
-    
-    "SALUD": [],
-    
-    "HOGAR": [],
-    
-    "MAPA": [],
-    
-    "MULTIMEDIA": [],
-    
-    "INSTRUMENTOS MUSICALES": [],
-    
-    "NÚMEROS Y CARACTERES": [],
-    
-    "PROFESIONES Y TRABAJOS": [],
-    
-    "PLANIFICACIÓN DE CARRETERAS": [],
-    
-    "ESCUELA": [],
-    
-    "ESPACIO": [],
-    
-    "HERRAMIENTAS": [],
-    
-    "SEÑALES DE TRÁFICO": [],
-    
-    "TRANSPORTE": [],
-    
-    "ÁRBOLES": [],
-    
-    "VEGETALES": []
-};
+let bd = new Array(8);
+bd[0] = ['PERA', 'GUAYABA', 'NARANJA', 'MELÓN', 'PIÑA','CEREZA', 'ALBARICOQUE'];
+bd[1] = ['PIMIENTO', 'CEBOLLA', 'BRÓCOLI', 'ENELDO', 'ZANAHORIA', 'PEPINO', 'HINOJO'];
+bd[2] = ['TURRÓN','CHUPETA','CHOCOLATE','GELATINA','HELADO','GALLETA','MENTA','REGALIZ'];
+bd[3] = ['ROSA', 'LIRIO', 'GIRASOL', 'AMAPOLA', 'NARCISO', 'GERANIO', 'LAVANDA'];
+bd[4] = ['OLIVO', 'HELECHO', 'PALMERA', 'BAMBÚ', 'ROBLE', 'PINO', 'ALMENDRO'];
+bd[5] = ['LEÓN', 'GALLINA', 'PERRO', 'LEOPARDO', 'MURCIÉLAGO', 'MONO', 'ELEFANTE'];
+bd[6] = ['HORNO', 'NEVERA', 'SARTÉN', 'BATIDORA', 'CUCHILLO', 'OLLA', 'TOSTADORA'];
+bd[7] = ['FLAUTA', 'ARMóNICA', 'PIANO', 'ARPA', 'TAMBORES', 'CASTAÑUELAS', 'BONGOS'];
+bd[8] = ['BORRA', 'REGLA', 'TINTA', 'TIJERAS', 'LÁPIZ', 'PEGA', 'BOLÍGRAFO'];
 
 
-bd[6] = ['HELICONIA', 'ORQUÍDEA', 'HELICONIA', 'LAVANDA', 'DALIA', 'HELICONIA', 'HIBISCO', 'GIRASOL', 'LIRIO TIGRE', 'TITHONIA', 'IXORA', 'FLOR DE LA PASIÓN', 'IRIS', 'LAELIA', 'DALIA', 'DIENTE DE LEÓN', 'AVE DEL PARAÍSO', 'BUGANVILLA', 'AMAPOLA MEXICANA', 'PEONÍA', 'CANTUTA', 'VIOLETA', 'LIRIO CALA', 'CAMPANILLA DE INVIERNO', 'ASTER', 'ÁRBOL DE CORAL ESPUELA DE GALLO', 'NO ME OLVIDES ALPINO', 'FRANGIPANI', 'CHUQUIRAGA', 'FLOR DE LOTO', 'LIRIO DEL AMAZONAS', 'CROCUS', 'CAMPANILLA AZUL', 'GLADIOLUS', 'LANTANA CAMARA', 'ROSA', 'BEGONIA', 'AMAPOLA', 'CAMPANILLA DE ESCOCIA', 'TULIPÁN', 'MARGARITA', 'VERAWOOD', 'GAILLARDIA', 'LIRIO', 'CEMPASÚCHIL', 'FLOR DE CEREZO', 'ALLIUM', 'NARCISO', 'BOUVARDIA', 'BROMELIA', 'TROMPETA DORADA', 'JACINTO', 'GERANIO', 'PALICOUREA ELATA', 'CRISANTEMO', 'POINSETTIA', 'RÚCULA', 'JABOROSA INTEGRIFOLIA', 'CACTUS', 'LIRIO TIGRE', 'CILANTRO', 'DIANTHUS FRINGED', 'MANZANILLA', 'DIANTHUS', 'MOSTAZA DE AJO','BORRAJA', 'CALÉNDULA DE MENTA', 'FLORES DE CEBOLLINO', 'PENSAMIENTO', 'DELPHINIUM', 'FLORES DE FRESA', 'GUISANTE', 'CAPUCHINA', 'MANZANILLA SALVAJE', 'ALSTROEMERIA', 'AMARILIS', 'ANÉMONA', 'ANTHURIUM', 'FRESIA'];
+let categorias = ['FRUTAS', 'VEGETALES', 'CARAMELOS','FLORES', 'ÁRBOLES','ANIMALES', 'HOGAR', 'MÚSICA', 'ESCUELA']
 
-bd[7] = ['BANANA', 'SANDÍA', 'CEREZA', 'COCO', 'CIRUELA', 'PAPAYA', 'GRANADA', 'PIÑA', 'LONGAN', 'MELÓN', 'FRAMBUESA', 'MANZANA', 'HIGOS', 'KIWI', 'ARÁNDANO', 'FRUTA DEL DRAGÓN', 'MANGO', 'GUAYABA', 'POMELO', 'DÁTIL', 'CAQUI', 'MEMBRILLO', 'PERA', 'AGUACATE', 'MANGOSTÁN', 'FRUTA DE LA PASIÓN', 'RAMBUTÁN', 'ALBARICOQUE', 'SAPODILLA', 'JACA', 'LIMA', 'CORRIENTE', 'CARAMBOLA', 'MELOCOTÓN', 'NÍSPERO', 'BERGAMOTA', 'CACAO', 'OLIVA', 'TOMATE', 'KUMQUAT', 'PITANGA', 'GUANÁBANA', 'TAMARILLO', 'PLÁTANO', 'UVA', 'KUMQUATS', 'NARANJA', 'FRESA', 'LIMÓN', 'MANDARINA'];
 
-bd[8] = ["VENDAJE","SANGRE","CÁPSULA","MULETA","GOTERO","MASCARILLA","FEMENINO","MASCULINO","BÁSCULA","ESTETOSCOPIO","JERINGA","TABLETA","TERMÓMETRO"];
+let palabrasIngles = [
+    ['PEAR', 'GUAVA', 'ORANGE', 'MELON', 'PINEAPPLE', 'CHERRY', 'APRICOT'],
+    ['BELL PEPPER', 'ONION', 'BROCCOLI', 'DILL', 'CARROT', 'CUCUMBER', 'FENNEL'],
+    ['NOUGAT', 'LOLLIPOP', 'CHOCOLATE', 'JELLY', 'ICE CREAM', 'COOKIE', 'MINT', 'LICORICE'],
+    ['ROSE', 'LILY', 'SUNFLOWER', 'POPPY', 'DAFFODIL', 'GERANIUM', 'LAVENDER'],
+    ['OLIVE', 'FERN', 'PALM', 'BAMBOO', 'OAK', 'PINE', 'ALMOND'],
+    ['LION', 'HEN', 'DOG', 'LEOPARD', 'BAT', 'MONKEY', 'ELEPHANT'],
+    ['OVEN', 'REFRIGERATOR', 'PAN', 'BLENDER', 'KNIFE', 'POT', 'TOASTER'],
+    ['FLUTE', 'HARMONICA', 'PIANO', 'HARP', 'DRUMS', 'CASTANETS', 'BONGOS'],
+    ['ERASER', 'RULER', 'INK', 'SCISSORS', 'PENCIL', 'GLUE', 'PEN'],
+]
 
-bd[9] = ['KIMCHI', 'LAVADORA', 'DELANTAL', 'WC', 'CAMA', 'SARTÉN', 'MICROONDAS', 'HORNO', 'COMPUTADORA', 'RALLADOR', 'COCINA', 'CUCHARÓN', 'CORTADOR', 'LÁMPARA', 'OLLA', 'RODILLO', 'CUBIERTOS', 'TAMIZ', 'COMIDA', 'TAZAS', 'LÁMPARA', 'RECETA', 'TELEVISIÓN', 'TEMPORIZADOR', 'GUANTE', 'SISTEMA DE SONIDO', 'SILLA DE ESCRITORIO', 'SAL', 'ALARMA', 'PARRILLA', 'TETERA', 'ESPEJO', 'MOPA', 'MANGA PASTELERA', 'CONTENEDOR DE COMIDA', 'TOSTADORA', 'SARTÉN', 'MESA', 'MESITA DE NOCHE', 'BATIDOR', 'PERCHA', 'ENSALADA', 'LICUADORA', 'COJINES', 'CUCHILLO', 'SOFÁ', 'HORNEAR', 'PARRILLA DE BARBACOA', 'TETERA', 'TERMÓMETRO', 'BIRYANI', 'LIBRERO', 'RELOJ DESPERTADOR', 'SOMBRERO DE CHEF', 'SILLA', 'SARTÉN', 'BAÑERA', 'MICROONDAS', 'COLADOR', 'ARMARIO', 'CORTINA', 'CUCHARA', 'CHIMENEA', 'PELADOR', 'PUERTA', 'NEVERA', 'EXPRIMIDOR', 'ALFOMBRA', 'MEZCLADOR', 'RECETA', 'PAPELERA', 'VENTILADOR', 'BÁSCULA', 'PAÑO', 'FREGADERO', 'KETCHUP', 'ENCHUFE', 'MEZCLADOR', 'ESTUFA', 'TAZA DE MEDIR', 'ROUTER', 'ESPÁTULA', 'TOSTADORA', 'PANQUEQUE', 'DISPENSADOR DE AGUA', 'CORTAR', 'TELÉFONO', 'MEZCLAR', 'HORNO', 'NATILLAS', 'TIMBRE', 'PLANTA', 'PALETA', 'AIRE ACONDICIONADO', 'CUCHILLO', 'ESCRITORIO', 'OLLA', 'CAFETERA', 'SACACORCHOS', 'CLAQUETA'];
+let palabrasItaliano = [
 
-bd[10] = [];
-
-bd[11] = ['BATERÍA', 'IPOD', 'ECUALIZADOR', 'ALTAVOZ', 'WEBCAM', 'TV', 'NEGATIVO', 'PENDRIVE', 'IPOD SHUFFLE', 'DISQUETE', 'PORTÁTIL', 'TELÉFONO MÓVIL', 'IMPRESORA', 'RATÓN', 'CÁMARA', 'MÓDEM', 'TABLETA', 'CORREO', 'MICRÓFONO', 'TARJETA DE MEMORIA', 'CHAT', 'JUEGO', 'CÁMARA', 'TV', 'ANTENA', 'ALTAVOZ', 'TECLADO', 'RADIO', 'NUBE', 'PAUSA', 'DISCO COMPACTO', 'REPRODUCIR', 'DETER', 'SIGUIENTE PISTA', 'PISTA ANTERIOR', 'REC', 'AURICULARES', 'CÁMARA', 'CÁMARA', 'PROYECTOR', 'CABLE USB', 'PELÍCULA NEGATIVA', 'DVD', 'MICRÓFONO', 'PELÍCULA', 'GAFAS D', 'CD'];
-
-bd[12] = ['ACORDEÓN', 'AMPLIFICADOR', 'BALALAIKA', 'BANJO', 'BONGOS', 'CABASA', 'CASTAÑUELAS', 'GRABACIÓN DE CD', 'CAMPANILLAS', 'CLAVE', 'COMPUTACIÓN EN LA NUBE', 'CONGA', 'CENCERRO', 'PLATILLOS', 'DIAPASÓN', 'TAMBOR', 'BAQUETAS', 'GUITARRA ELÉCTRICA', 'ECUALIZADOR', 'FLAUTA', 'CARPETA', 'GONG', 'GUITARRA', 'ARMÓNICA', 'AURICULARES', 'IPOD', 'TECLADO', 'KEYTAR', 'MARACAS', 'MÉTRONOMO', 'MICRÓFONO', 'BOLA DE ESPEJOS', 'NOTA MUSICAL', 'SILENCIADO', 'CORCHEA', 'TELÉFONO INTELIGENTE', 'PANDERETA', 'TRIÁNGULO', 'TROMPETA', 'TORNAMESA', 'VOLUMEN', 'XILÓFONO'];
-
-bd[15] = [ "DESTINO","VEHÍCULO","POSICIÓN","ESTRELLA","BOSQUE","INTERSECCIÓN","HOGAR","GPS","UBICACIÓN","MAPA","PUNTO DE INTERÉS","CARRETERA","ROTONDA","OBJETIVO","CARRETERA DE PEAJE","TRABAJO","MARCA","FLECHA ARRIBA","MARCADOR DE UBICACIÓN","PLANO","AUTOPISTA","APROBADO","RUTAS","SEÑALIZACIÓN","TÚNEL","CRUCE","INTERSECCIÓN EN T","RUTA","NAVEGACIÓN","PUENTE","VÉRTICE","CONO DE TRÁFICO","LÁPIZ","ENSANCHAMIENTO","CALLES","HOGARES","SÍMBOLO MENOS"];
+];
 
 
 const cantidadPalabras = 5;
-
 let palabras = [];
-
 let desordenadas = [];
 
 let pos = 0;
@@ -86,7 +50,6 @@ agregarPalabras(bd[pos]);
 
 function desordenarPalabras(){
     for(var i=0;i<palabras.length;i++){
-      
         let palabra = palabras[i];
         palabra = palabra.split('');
     
@@ -95,23 +58,25 @@ function desordenarPalabras(){
         palabraDesordenada = palabra.sort(function(){return Math.random() - 0.5});
     
         palabraDesordenada = palabraDesordenada.toString();
-
         palabraDesordenada = palabraDesordenada.replace(/,/g,"");
-      
+    
         if(palabraDesordenada == palabras[i]){
             i = i - 1;
         }else{
-            
             desordenadas.push(palabraDesordenada);
         }
     }
 }
 
-
-function agregarPalabra(){
-  
+function agregarPalabra() {
     let h2 = document.createElement("h2");
-    h2.textContent = categorias[pos];
+    if (idiomaElegido === "Inglés") {
+        h2.textContent = "FRUITS"; 
+    } else if (idiomaElegido === "Italiano") {
+        h2.textContent = "FRUTTA"; 
+    } else {
+        h2.textContent = categorias[pos]; 
+    }
     document.querySelector("#contenedor").appendChild(h2);
     for(var i = 0; i < desordenadas.length;i++){
         let div = document.createElement("div");
@@ -122,7 +87,6 @@ function agregarPalabra(){
         div.appendChild(palabra);
         let input = document.createElement("input");
         input.id = i;
-     
         input.setAttribute("onkeyup", "corregir("+i+")");
         div.appendChild(input);
         document.querySelector("#contenedor").appendChild(div);
@@ -133,23 +97,22 @@ desordenarPalabras();
 agregarPalabra();
 efectoNivel();
 
-
 function corregir(i) {
-    let input = document.getElementById(i);
-    let texto = input.value; 
-  
-    let textoMayusculas = texto.toUpperCase();
-  
-    input.value = textoMayusculas;
-  
-    if (textoMayusculas === palabras[i]) {
-      input.className = "correcta";
-      controlarFin();
-    } else {
-      input.className = "";
-    }
+  let input = document.getElementById(i);
+  let texto = input.value;
+
+  let textoMayusculas = texto.toUpperCase();
+
+  input.value = textoMayusculas;
+
+  if (textoMayusculas === palabras[i]) {
+    input.className = "correcta";
+    controlarFin();
+  } else {
+    input.className = "";
   }
-  
+}
+
 
 let btnCraeado = false;
 function controlarFin(){
@@ -161,7 +124,6 @@ function controlarFin(){
         document.querySelector("#contenedor").appendChild(button);
         btnCraeado=true;
         
-    
         let niveles = document.getElementsByClassName("nivel");
         niveles[pos].classList = "nivel completado";
     }
@@ -172,7 +134,7 @@ function siguiente(){
     desordenadas.length = 0;
     document.querySelector("#contenedor").textContent = "";
     pos++;
- 
+
     if(pos<bd.length){
         btnCraeado = false;
         agregarPalabras(bd[pos]);
